6d66bf95fb05803074ff1cb72f2a881c5364ab1f
angelicapan <angelica.pan@berkeley.edu>
2018-04-25 00:49:20 -0700

    Add files via upload

--- assets/js/js.js
+++ assets/js/js.js
@@ -24,8 +24,34 @@ $(document).ready(function() {
 	//jQuery Function Number 6
 	$(".item").click(function() {
    		// jQuery Function Number 7
-   		
-   			$(".center").text("Click on more memories!");
+   		if ($(this).hasClass("1")){
+   			$(".center").text("I saw this cool toadstool in front of crossroads and put a coin on top of it. I wanted to see how long it would stay there but I forgot to check.");	
+   		}
+   		if ($(this).hasClass("2")){
+   			$(".center").text("I learned to like eating salads this year.");	
+   		}
+   		if ($(this).hasClass("3")){
+   			$(".center").text("Golden hour! This is the view from Soda Hall");	
+   		}
+   		if ($(this).hasClass("4")){
+   			$(".center").text("A nice day spent catching Pokemon on Telegraph!");	
+   		}
+   		if ($(this).hasClass("5")){
+   			$(".center").text("I went to a farmers' market in San Jose and didn't know what Jujubes were. They have funny names. ");	
+   		}
+   		if ($(this).hasClass("6")){
+   			$(".center").text("He ties his shoe lace weird. ");	
+   		}
+   		if ($(this).hasClass("7")){
+   			$(".center").text("One of the rare times where I wake up early enough to get the breakfast menu at the Den");	
+   		}
+   		if ($(this).hasClass("8")){
+   			$(".center").text("Cute birds by a house the day we went to Corgi Con");	
+   		}
+   		if ($(this).hasClass("9")){
+   			$(".center").text("I stayed in SoCal with my roommate over winter break and we went to a cute cafe!");	
+   		}
+   			
    			
    	
    		
